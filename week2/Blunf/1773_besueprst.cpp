18a3d503a4795bf57ca00393519d483a555ee289
Blunf <besuperst@gmail.com>
2023-05-12 21:01:25 +0900

    test2

---
+++ week2/Blunf/1773_besueprst.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+ 
+int main(){
+	int pnum, totaltime;
+	std::scanf("%d %d", &pnum, &totaltime);
+
+	int count=0;
+	int arr[pnum];
+	for(int i=0;i<pnum;i++) cin >> arr[i];
+	
+	int ans[totaltime];
+	for(int i=0;i<pnum; i++){
+		for(int k=0;k<totaltime/arr[k];k++){
+			if(ans[(k+1)*arr[i]-1]!=1){ans[(k+1)*arr[i]-1]=1;}
+		}
+	}
+	for(int i=0;i<totaltime;i++){
+		if(ans[i]==1){count++;}
+	}
+	cout << count;
+
+	return 0;
+}
