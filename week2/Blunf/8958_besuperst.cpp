1725f4570d3e670e54f167b4db5ca355cb4a70f7
Blunf <besuperst@gmail.com>
2023-05-16 12:57:12 +0900

    week2end

--- week2/Blunf/8958_besuperst.cpp
+++ week2/Blunf/8958_besuperst.cpp
@@ -1,5 +1,22 @@
 #include<iostream>
 
+using namespace std;
+
 int main(){
-    
+    int cnt;
+    cin >> cnt;
+    string list[cnt];
+
+    for(int i=0;i<cnt;i++) cin >> list[i];
+
+    for(int i=0;i<cnt;i++){
+        int total =0;
+        int k=1;
+        for(char c : list[i]){
+            if(c == 'O'){total=total+k; k++;}
+            else{k=1;}
+        }
+        cout<<total;
+        cout<<"\n";
+    }
 }
